be0d6a8e07e8c0e797f82ff78bc1a088fa92e0a0
AomenX <amine.momenn@outlook.com>
2025-09-27 12:21:56 -0700

    task 01/16 of 0x01 in C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * prints the alphabet, in lowercase, followed by a new line
+ * Prototype: void print_alphabet(void),
+ * You can only use _putchar twice in your code
+ * Return: always 0 (success)
+ * */
+void	print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}

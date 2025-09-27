8e92c37d84503826ca41ef5a98d295183ceb13d5
AomenX <amine.momenn@outlook.com>
2025-09-27 12:53:38 -0700

    task 03/16 of 0x02 in C

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - function that checks for lowercase character.
+ * Prototype: int _islower(int c);
+ * Returns 1 if c is lowercase
+ * Returns 0 otherwise
+ */
+int	_islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}

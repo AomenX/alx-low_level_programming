3f0f9f707fb683a2dd98e80bef3263a551bae9cd
AomenX <amine.momenn@outlook.com>
2025-09-27 14:51:51 -0700

    task 08/16 of 0x02 in C

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
@@ -7,8 +6,8 @@
 void	jack_bauer(void)
 {
 	int 	i;
-       	int	j;
-
+	int	j;
+	
 	i = 0;
 	while (i < 24)
 	{

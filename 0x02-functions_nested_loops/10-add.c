a3347929e5420609be86cbaa9d3e1f6d0bd95391
AomenX <amine.momenn@outlook.com>
2025-09-27 15:42:46 -0700

    task 10/16 of 0x02 in C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * Prototype: int add(int x, int y);
+ * @x: int to be added to y
+ * @y: int to be added to x
+ * Return: sum of a and b
+ */
+int	add(int x, int y)
+{
+	return (x + y);
+}

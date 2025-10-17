2fa8f543f355d65b8e6225b8a6ebb1744421cb59
AomenX <amine.momenn@outlook.com>
2025-10-17 09:40:45 -0700

    task 05/13 of 0x04 in C

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * followed by a new line
+ * Do not print 2 and 4
+ */
+
+void	print_most_numbers(void)
+{
+	int	c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		if (c == '2' || c == '4')
+			continue;
+		_putchar(c);
+	}
+	_putchar('\n');
+}

626434fdc788176d06eb0d2cf7a1c1346029dc50
AomenX <amine.momenn@outlook.com>
2025-10-23 21:33:55 -0700

    task 06/12 of 0x05 in C

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * Prototype: void rev_string(char *s);
+ * @s: string to be reversed
+ */
+void	rev_string(char *s)
+{
+	int	i;
+	int	len;
+	char	tmp;
+
+	i = 0;
+	while (s[i])
+		i++;
+	len = i - 1;
+	i = 0;
+	while (i < len / 2)
+	{
+		tmp = s[i];
+		s[i] = s[len];
+		s[len] = tmp;
+		i++;
+		len--;
+	}
+}

2ae03ab37e08b8ffdb3ab919e449cc9297889b65
AomenX <amine.momenn@outlook.com>
2025-10-25 17:20:48 -0700

    task 10/12 of 0x05 in C

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * Prototype: char *_strcpy(char *dest, char *src);
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char	*_strcpy(char *dest, char *src)
+{
+	int	i, l;
+
+	l = 0;
+	while (src[l])
+		l++;
+	i = 0;
+	while(i < l)
+	{
+		dest[i++] = src[i++];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
